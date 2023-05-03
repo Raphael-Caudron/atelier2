05f1927f756a489328c8ad262eb68de367890866
Arthur Jézéquel <42878548+Cubebleu1@users.noreply.github.com>
2023-05-03 17:58:48 +0200

    Add files via upload

--- SPAtelier2/src/main/java/com/sp/model/Card.java
+++ SPAtelier2/src/main/java/com/sp/model/Card.java
@@ -7,5 +7,28 @@ import javax.persistence.Id;
 
 @Entity
 public class Card {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String superPowerName;
+	private int superPowerValue;
+	private int hp;
+	private String description;
+	private String imgUrl;
+	
+	public Card() {
+		
+	}
+	
+	public Card(int id, String name, String superPowerName, int superPowerValue, int hp, String description, String imgUrl) {
+		this.id = id;
+		this.name = name;
+		this.superPowerName = superPowerName;
+		this.superPowerValue = superPowerValue;
+		this.hp = hp;
+		this.description = description;
+		this.imgUrl = imgUrl;
+	}
 
 }
--- SPAtelier2/src/main/java/com/sp/repository/CardRepository.java
+++ SPAtelier2/src/main/java/com/sp/repository/CardRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.sp.model.Card;
 
 public interface CardRepository extends CrudRepository<Card, Integer> {
-
+	
+	public List<Card> findByName(String name);
+	
+	public Card findById(int id);
+	
+	public Card save(Card c);
 }
