c7d84c59d85868ed982d1b853435a91a97cf550a
Raphael <raphael0450@gmail.com>
2023-05-05 08:36:25 +0200

    commit auth

---
+++ SPAtelier2/src/main/java/com/sp/rest/AuthentificationRestController.java
@@ -0,0 +1,24 @@
+package com.sp.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sp.model.User;
+//import com.sp.model.Auth;
+import com.sp.service.AuthentificationService;
+
+@RestController
+public class AuthentificationRestController {
+	
+	@Autowired
+	AuthentificationService aService;
+	
+	@RequestMapping(method=RequestMethod.POST, value="/login")
+	public void login(@RequestBody User user) {
+		aService.login(user);
+	}
+}
---
+++ SPAtelier2/src/main/java/com/sp/service/AuthentificationService.java
@@ -0,0 +1,23 @@
+package com.sp.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.sp.model.User;
+
+//import com.sp.model.Auth;
+
+@Service
+public class AuthentificationService {
+	
+	@Autowired
+	UserService uService;
+	
+	public void login(User user) {
+		uService.getUser(user);
+	}
+
+}
