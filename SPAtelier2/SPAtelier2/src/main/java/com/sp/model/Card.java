7e00409617437ef3d3f4dd574e92c2b81e179203
Thibault Berthet <91309781+Thibaultbtt@users.noreply.github.com>
2023-05-10 16:27:47 +0200

    tr

---
+++ SPAtelier2/SPAtelier2/src/main/java/com/sp/model/Card.java
@@ -0,0 +1,11 @@
+package com.sp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Card {
+
+}
---
+++ SPAtelier2/SPAtelier2/src/main/java/com/sp/repository/CardRepository.java
@@ -0,0 +1,9 @@
+package com.sp.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.sp.model.Card;
+
+public interface CardRepository extends CrudRepository<Card, Integer> {
+
+}
