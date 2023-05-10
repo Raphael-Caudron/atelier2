0073dd6a7465a6cd840e38f7fc1e53370ad7e540
Thibault Berthet <91309781+Thibaultbtt@users.noreply.github.com>
2023-05-10 17:20:25 +0200

    Add files via upload

--- SPAtelier2/src/main/java/com/sp/repository/UserRepository.java
+++ SPAtelier2/src/main/java/com/sp/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.sp.repository;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import com.sp.model.User;
 
-public interface UserRepository {
-
+public interface UserRepository extends CrudRepository<User, Integer>{
+	public User findByUsername(String username);
 }
